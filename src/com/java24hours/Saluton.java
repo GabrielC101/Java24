0ea2dc3db36bca9995caeb739d703ebcf05e680f
Gabriel Curio <g.curio@gmail.com>
2016-05-09 17:06:46 -0500

    Initial commit. Have already begun project.

---
+++ src/com/java24hours/Saluton.java
@@ -0,0 +1,10 @@
+package com.java24hours;
+
+class Saluton {
+    public static void main(String[] arguments) {
+        String greeting  = "Saluton mondo!";
+        System.out.println(greeting);
+        
+        
+    }
+}
---
+++ src/com/java24hours/Variable.java
@@ -0,0 +1,20 @@
+package com.java24hours;
+
+class Variable {
+    public static void main(String[] arguments) {
+        int tops;
+        float gradePointAverage;
+        char key = 'C';
+        String productName = "Larvets";
+        
+        byte escapeKey = 27;
+        short roomNumber = 222;
+        
+        long salary = 264_400_000;
+        boolean gameOver = false;
+        
+        
+        
+        
+    }
+}
