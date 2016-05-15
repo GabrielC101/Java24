8c54dd969a8a72d1c0be7121965a0e0ca095726e
Gabriel Curio <g.curio@gmail.com>
2016-05-14 21:08:17 -0500

    Added NewRoot object.

---
+++ src/com/java24hours/NewRoot.java
@@ -0,0 +1,14 @@
+package com.java24hours;
+
+class NewRoot {
+    public static void main(String[] arguments) {
+  
+        int number = Integer.parseInt(arguments[0]);
+        System.out.println("The square root of "
+                + number
+                + " is "
+                + Math.sqrt(number)
+        );
+    }
+    
+}
